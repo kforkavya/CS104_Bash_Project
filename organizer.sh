c248e33c8a0144c26e11df15239fbc0d44ea722f
kforkavya <120742583+kforkavya@users.noreply.github.com>
2023-06-04 16:08:01 +0530

    Renaming done via a function for better points
    Renaming for recurring files in directory for both sort by extension and sort by name done by function

--- organizer.sh
+++ organizer.sh
@@ -7,84 +7,87 @@ touch output.txt # keeping track of files transferred
 touch temp.txt
 
 # Checking if the destination directory doesn't exist, then create a new one
-if [ ! -d $desdir ]; then
-  mkdir $desdir
+if [ ! -d "$desdir" ]; then
+  mkdir "$desdir"
   echo "Destination Directory $desdir does not exist. No worries, created one." >> output.txt
 fi
 
-# Now extracting files and checking their extensions
-for i in $(find $srcdir -type f); do
-  filename=$(basename $i)
-  name=$(echo $filename | sed 's/\([^\.]*\)\.[^\.]*/\1/') # name without extension
-  ext=$(echo $filename | sed 's/[^\.]*\.\([^\.]*\)/\1/') # ext is extension
-  if [ -n $ext ]; then # -n ensures that the string returned is non-empty
-    extdir="Extension_."$ext # extdir is the extension directory
+function recurring_filename {
+  dir=$1 # dir is the directory where multiple names are clashing
+  recur_name=$2 # recur_name is the recurring filename
+  ext=$3 # ext is the extension of recur_name
+  j=1
+  newfilepath=$desdir"/"$dir"/"$recur_name"."$ext # newfilepath is the new name for a repeated filename in the same extdir
+  while [ -e "$newfilepath" ]; do
+    new_newfilepath=$desdir"/"$dir"/"$recur_name"_"$j"."$ext
+    let "j=j+1"
+    newfilepath=$new_newfilepath
+  done
+  # now we have got the new name for a recurring filepath and echo it
+  echo "$newfilepath"
+}
+
+# Now extracting files and checking their extensions and creation time
+for i in $(find "$srcdir" -type f); do
+  filename=$(basename "$i")
+  name=$(echo "$filename" | sed 's/\([^\.]*\)\.[^\.]*/\1/') # name without extension
+  ext=$(echo "$filename" | sed 's/[^\.]*\.\([^\.]*\)/\1/') # ext is extension
+  if [ -n "$ext" ]; then # -n ensures that the string returned is non-empty
+    extdir="Extension_.$ext" # extdir is the extension directory
   else
     extdir="No_Extension"
   fi
 
   # check if extdir exists or not
-  if [ ! -e $desdir/$extdir ]; then
+  if [ ! -e "$desdir/$extdir" ]; then
     let "folder_created=folder_created+1"
-    mkdir $desdir/$extdir # create a new extdir
+    mkdir "$desdir/$extdir" # create a new extdir
     echo "A new extension folder $extdir is created" >> output.txt
   fi
 
   # Keeping track of the times when which folders are used
   echo "$extdir" >> temp.txt
   # Now checking if the same filename already exists
-  newfilepath=$desdir"/"$extdir"/"$filename
-  if [ ! -e $newfilepath ]; then
-    cp $i $desdir/$extdir
+  newfilepath="$desdir/$extdir/$filename"
+  if [ ! -e "$newfilepath" ]; then
+    cp "$i" "$desdir/$extdir"
     echo "Added $filename to $extdir folder" >> output.txt
   else
-    j=1
-    new_newfilepath=$newfilepath # new_newfilepath is the new name for a repeated filename in the same extdir
-    while [ -e $new_newfilepath ]; do
-      new_newfilepath=$desdir"/"$extdir"/"$name"_"$j"."$ext
-      let "j=j+1"
-    done
-    # now we have got the new name for a recurring filename
-    cp $i $new_newfilepath # this copies the recurring file with the correct name
-    newname=$(basename $new_newfilepath)
+    new_newfilepath=$(recurring_filename "$extdir" "$name" "$ext")
+    newname=$(basename "$new_newfilepath")
+    cp "$i" "$new_newfilepath" # this copies the recurring file with the correct name
     echo "$filename already exists, so renamed to $newname and stored in $extdir directory" >> output.txt
   fi
-done
 
-# Now extracting files and checking their creation time
-for i in $(find $srcdir -type f); do
-  filename=$(basename $i)
-  name=$(echo $filename | sed 's/\([^\.]*\)\.[^\.]*/\1/') # name without extension
-  extdir=$(echo $filename | sed 's/[^\.]*\.\([^\.]*\)/\1/') # extdir is the extension directory
-  ctime=$(stat $i | sed -n '/Birth/p' |  awk 'BEGIN{FS=" "}{print $2}' | awk 'BEGIN{FS="-"}{print $3$2$1}') # ctime is creation time
+  # Checking creation time
+  ctime=$(stat "$i" | sed -n '/Birth/p' |  awk 'BEGIN{FS=" "}{print $2}' | awk 'BEGIN{FS="-"}{print $3$2$1}') # ctime is creation time
 
   # check if ctime exists or not
-  if [ ! -e $desdir/$ctime ]; then
+  if [ ! -e "$desdir/$ctime" ]; then
     let "folder_created=folder_created+1"
-    mkdir $desdir/$ctime # create a new extdir
+    mkdir "$desdir/$ctime" # create a new date folder
     echo "A new date folder $ctime is created" >> output.txt
   fi
 
   # Keeping track of the times when which folders are used
   echo "$ctime" >> temp.txt
   # Now checking if the same filename already exists
-  newfilepath=$desdir"/"$ctime"/"$filename
-  if [ ! -e $newfilepath ]; then
-    cp $i $desdir/$ctime
+  newfilepath="$desdir/$ctime/$filename"
+  if [ ! -e "$newfilepath" ]; then
+    cp "$i" "$desdir/$ctime"
     echo "Added $filename to $ctime date folder" >> output.txt
   else
-    ctime=$(stat $i | sed -n '/Birth/p' |  awk 'BEGIN{FS=" "}{print $2}' | awk 'BEGIN{FS="-"}{print $3$2$1}') # ctime is creation time
-    # the above expression returns the creation time of the file
-    newname=$name"_"$ctime"."$extdir
-    cp $i $desdir/$ctime/$newname # this copies the file with a timestamp
+    new_newfilepath=$(recurring_filename "$ctime" "$name" "$ext")
+    newname=$(basename "$new_newfilepath")
+    cp "$i" "$new_newfilepath" # this copies the recurring file with the correct name
     echo "$filename already exists, so renamed to $newname and stored in $ctime date directory" >> output.txt
   fi
 done
 
-echo "Folders created: "$folder_created
+echo "Folders created: $folder_created"
 cat output.txt
 echo "Number of files in each folder:-"
-awk '{Grp[$1]++} END {for (i in Grp) print i":"Grp[i]}' temp.txt # This command prepares a dictionary of extensions as keys and frequencies as values
+awk '{Grp[$1]++} END {for (i in Grp) print i":"Grp[i]}' temp.txt # This command prepares a dictionary of extensions/creation dates as keys and frequencies as values
 rm output.txt
 rm temp.txt
 rm folder_count.txt
